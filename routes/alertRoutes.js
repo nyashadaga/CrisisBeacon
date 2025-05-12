c88ec633624954b37545cf7ac10233b83e408a58
Nyasha <your-github-nyashadaga@example.com>
2025-05-13 04:10:04 +0530

    before-break

--- routes/alertRoutes.js
+++ routes/alertRoutes.js
@@ -4,6 +4,7 @@ const authenticateJWT = require('../middlewares/authMiddleware')
 
 const alertController = require('../controllers/alertController');
 
+
 router.post('/', authenticateJWT, alertController.createAlert);
 router.delete('/:id', authenticateJWT, alertController.deleteAlert);
 router.get('/', authenticateJWT, alertController.getAlerts);
